3438b372d3f061050f524b276cb06df7d7e835ce
mrivh <m.alicia.riv@gmail.com>
2021-08-18 15:07:11 -0500

    Search bar

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: ["regenerator-runtime/runtime.js",'./src/js/index.js'],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
